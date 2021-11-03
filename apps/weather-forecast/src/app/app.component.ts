55c876bd30c65ed12ee1530de5b7a5f5b1be3d65
tzztson <zhenghu61919@gmail.com>
2021-11-03 21:24:24 +0200

    feat: remove onpush

--- apps/weather-forecast/src/app/app.component.ts
+++ apps/weather-forecast/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'bp-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
 	title = 'weather-forecast';
