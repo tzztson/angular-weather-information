cdcca9a9cfc87cf0a67d1eb934ea9c0eb816a127
tzztson <zhenghu61919@gmail.com>
2021-12-23 07:46:56 -0800

    feat(lib): add function for forecast and define types

--- libs/weather-forecast/services/src/lib/weather-forecast-api.service.ts
+++ libs/weather-forecast/services/src/lib/weather-forecast-api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
-import { Coordinate } from './weather-forecast';
+import { Coordinate, ForecastMode, WeatherForecast } from './weather-forecast';
 
 @Injectable()
 export class WeatherForecastApiService {
@@ -15,14 +15,27 @@ export class WeatherForecastApiService {
 	) {
 	}
 
-	getCoordinates(city: string): Observable<Coordinate[]> {
+	getCoordinates(city: string): Observable<Coordinate> {
 		const url = `${this.url}/geo/1.0/direct`;
 		const params = new HttpParams()
 			.append('q', city)
 			.append('limit', 1)
 			.append('appid', this.apiKey);
 
-		return this.http.get<Coordinate[]>(url, { params });
+		return this.http.get<Coordinate[]>(url, { params }).pipe(
+			map(res => res[0])
+		);
+	}
+
+	getWeatherForecast(lat: number, lon: number, forecastMode: ForecastMode): Observable<WeatherForecast> {
+		const url = `${this.url}/data/2.5/onecall`;
+		const params = new HttpParams()
+			.append('lat', lat)
+			.append('lon', lon)
+			.append('exclude', `current,minutely,${forecastMode === ForecastMode.Daily ? 'hourly' : 'daily'},alerts`)
+			.append('appid', this.apiKey);
+
+		return this.http.get<WeatherForecast>(url, { params });
 	}
 
 }
--- libs/weather-forecast/services/src/lib/weather-forecast.ts
+++ libs/weather-forecast/services/src/lib/weather-forecast.ts
@@ -1,3 +1,8 @@
+export enum ForecastMode {
+	Hourly = 'HOURLY',
+	Daily = 'DAILY'
+}
+
 export interface Coordinate {
 	country: string;
 	lat: number;
@@ -5,4 +10,65 @@ export interface Coordinate {
 	name: string;
 	state: string;
 	local_names: { [key: string]: string };
+}
+
+export interface WeatherClip {
+	description: string;
+	icon: string;
+	id: number;
+	main: string;
+}
+
+export interface Day {
+	morn: number;
+	day: number;
+	eve: number;
+	night: number;
+}
+
+export interface TempOfDay extends Day {
+	max: number;
+	min: number;
+}
+
+export interface Weather {
+	clouds: number;
+	dew_point: number;
+	dt: number;
+	humidity: number;
+	pop: number;
+	pressure: number;
+	uvi: number;
+	weather: WeatherClip[];
+	wind_deg: number;
+	wind_gust: number;
+	wind_speed: number;
+}
+
+export interface WeatherHourly extends Weather {
+	feels_like: number;
+	temp: number;
+	visibility: number;
+}
+
+export interface WeatherDaily extends Weather {
+	feels_like: Day;
+
+	moon_phase: number;
+	moonrise: number;
+	moonset: number;
+
+	sunrise: number;
+	sunset: number;
+
+	temp: TempOfDay;
+}
+
+export interface WeatherForecast {
+	lat: number;
+	lon: number;
+	timezone: string;
+	timezone_offset: number;
+	hourly?: WeatherHourly[];
+	daily?: WeatherDaily[];
 }
