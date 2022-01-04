5d9fd11748bc34435a65052720b2b38e69d6ccce
tzztson <zhenghu61919@gmail.com>
2022-01-04 11:28:56 -0800

    feat(app): prepare main layout

---
+++ apps/weather-forecast/src/app/layout/layout.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutComponent } from './layout.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+	declarations: [
+		LayoutComponent,
+  NavbarComponent,
+	],
+	imports: [
+		CommonModule,
+	],
+	exports: [
+		LayoutComponent
+	]
+})
+export class LayoutModule {
+}
